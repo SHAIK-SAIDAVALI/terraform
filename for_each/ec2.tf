c10d2634c19a5aecdc4c2c4be64b33b6b3bb1e67
shaiksaidavali <shaiksaidavali223@gmail.com>
2026-08-25 01:39:14 +0530

    terraform

---
+++ for_each/ec2.tf
@@ -0,0 +1,31 @@
+resource "aws_instance" "Frontend" {
+    ami = "ami-0220d79f3f480ecf5"
+    for_each = var.instances
+    instance_type = each.value
+    vpc_security_group_ids = [ aws_security_group.allow_all.id ]
+    tags = {
+        Name = each.key
+        Terraform = "true"
+    }
+}
+
+resource "aws_security_group" "allow_all" {
+  name   = "allow_all"
+
+  egress {
+    from_port       = 0 ## All Ports ##
+    to_port         = 0  
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]      #internet
+  }
+
+  ingress {
+    from_port       = 0   ## All Ports ##
+    to_port         = 0
+    protocol        = "-1" # All Protoclos #
+    cidr_blocks     =   ["0.0.0.0/0"]  #internet
+  }
+  tags = {
+    Name= "allow_all"
+  }
+}
