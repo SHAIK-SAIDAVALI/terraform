da35c1550c7677726f2833e540ea19affb6358a6
shaiksaidavali <shaiksaidavali223@gmail.com>
2026-08-24 21:46:26 +0530

    terraform

--- EC2/ec2.tf
+++ EC2/ec2.tf
@@ -1,9 +1,10 @@
-resource "aws_instance" "Frontend" {
+resource "aws_instance" "frontend" {
     ami = "ami-0220d79f3f480ecf5"
     instance_type = "t3.micro"
-    vpc_security_group_ids = [ aws_security_group.allow_all.id ]
+    vpc_security_group_ids = [ "sg-0064149a4e8eb98ec" ]
+    
     tags = {
-        Name = "Frontend"
+        Name = "myvm"
         Terraform = "true"
     }
 }
