e22b2597b428a3ff9cc25a8fea49df01258b74b1
shaiksaidavali <shaiksaidavali223@gmail.com>
2026-08-25 03:31:54 +0530

    terraform

---
+++ Data-resources/data.tf
@@ -0,0 +1,31 @@
+data "aws_ami" "joindevops" {
+    owners           = ["973714476881"]
+    most_recent      = true
+    
+    filter {
+        name   = "name"
+        values = ["Redhat-9-DevOps-Practice"]
+    }
+
+    filter {
+        name   = "root-device-type"
+        values = ["ebs"]
+    }
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+}
+
+output "ami_id" {
+    value = data.aws_ami.joindevops.id
+}
+
+data "aws_instance" "terraform" {
+    instance_id = "i-0808a94a2de282a9e"
+}
+
+output "terraform_publicip" {
+    value = data.aws_instance.terraform.public_ip
+}
